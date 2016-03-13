9a3e281261c7372c542d8c9c216a48fb3d1c081c
Sergio Bobillier <sergio.bobillier@gmail.com>
2016-03-13 12:12:01 -0500

    Adds the character's experience and level (up/down) logic.

--- character.rb
+++ character.rb
@@ -4,7 +4,104 @@ require_relative "exceptions/character_not_in_party_exception"
 #
 # @author Sergio Bobillier <sergio.bobillier@gmail.com>
 class Character
+  # Level cap
+  MAX_LEVEL = 50
+
+  # Experience needed to reach level 2. The experience needed for level up is
+  # calculated using this figure as a base.
+  BASE_EXP = 100
+
+  # @return [Party] The party the character belongs to.
   attr_accessor :party
+
+  # @return [Integer] The character's level
+  attr_reader :level
+
+  # @return [Integer] The experience needed to reach the next level
+  attr_reader :next_level
+
+  # @return [Integer] The experience gained in the current level
+  attr_reader :experience
+
+  def initialize(level = 1)
+    # NOTE: Setting the level should be the last thing in the initializer
+    self.level = level
+  end
+
+  # Sets the character's level.
+  #
+  # @param level [Integer] The character's level.
+  # @raise [ArgumentError] If `level` is not an integer or ir is less than one
+  #   or greater than MAX_LEVEL.
+  def level=(level)
+    raise ArgumentError.new("`level` must be an Integer") unless level.is_a?(Integer)
+    raise ArgumentError.new("`level` must be between 1 and #{MAX_LEVEL}") unless level >= 1 && level <= MAX_LEVEL
+
+    @level = level
+    @experience = 0
+
+    # Calculates the experience needed to reach the next level. Should be 1.5
+    # times the experience needed to reach the current level.
+
+    experience = BASE_EXP
+
+    if level >= 2
+      (level-1).times { experience = (experience * 1.5).floor }
+    end
+
+    @next_level = experience
+  end
+
+  # Sets the character's experience in the current level. If the experience set
+  # is greater than the experience needed to level up then the character will
+  # level up and the remaining experience will be awarded for the next level. If
+  # the experience is negative then the character will level down and the
+  # remaining experience will be substracted from the previous level.
+  #
+  # @param experience [Integer] The experience.
+  # @raise [ArgumentError] If the experience is not an integer.
+  def experience=(experience)
+    raise ArgumentError.new("`experience` should be an integer") unless experience.is_a?(Integer)
+    return if experience == 0
+
+    if experience > 0
+      if experience < @next_level
+        @experience = experience
+      else
+        while experience >= @next_level && @level < MAX_LEVEL do
+          experience -= @next_level
+          self.level += 1
+        end
+
+        if experience > @next_level
+          @experience = @next_level
+        else
+          @experience = experience
+        end
+      end
+    else
+      if @level == 1
+        @experience = 0           # Character cannot go below level 1
+        return                    # so bail out.
+      end
+
+      experience = @experience + experience.abs
+
+      while experience >= @experience
+        experience -= @experience
+        self.level -= 1
+        @experience = @next_level
+
+        break if @level == 1      # Don't allow the charater to go below level 1
+      end
+
+      if experience > @experience
+        @experience = 0
+      else
+        @experience -= experience
+      end
+    end
+  end
 
   # Leaves the current party.
   #
--- tests/test_character.rb
+++ tests/test_character.rb
@@ -34,4 +34,118 @@ class TestCharacter < Minitest::Unit::TestCase
       character.leave_party
     end
   end
+
+  # Tests that a character can be created with a specific level and that the
+  # experience needed to reach the next level is properly calculated.
+  def test_creation_with_level
+    assert_raises ArgumentError do
+      Character.new("hello")                 # Not an integer
+    end
+
+    assert_raises ArgumentError do
+      Character.new(0)                      # Less than 1
+    end
+
+    assert_raises ArgumentError do
+      Character.new(Character::MAX_LEVEL+1)  # Above MAX_LEVEL
+    end
+
+    character = Character.new
+    assert_equal(1, character.level, "Character's level on creation should be 1")
+    assert_equal(Character::BASE_EXP, character.next_level, "Needed experience in level 1 should be equal to `BASE_EXP`")
+
+    character = Character.new(5)
+    assert_equal(5, character.level, "Character's level should be 5")
+    assert_equal(505, character.next_level, "Needed experience in level 5 should be equal to 505")
+
+    character = Character.new(Character::MAX_LEVEL)
+    assert_equal(Character::MAX_LEVEL, character.level, "Character's level should be #{Character::MAX_LEVEL}")
+  end
+
+  # Tests that the experience needed for the next level is properly calculated
+  # when character's level is set.
+  def test_experience_calculated_when_level_set
+    character = Character.new(2)
+    assert_equal(0, character.experience, "Experience should be set to 0 when character level is set")
+    assert_equal(150, character.next_level, "Experience needed to reach level 3 should be 150")
+
+    character.level = 1
+    assert_equal(0, character.experience, "Experience should be set to 0 when character level is set")
+    assert_equal(100, character.next_level, "Experience needed to reach level 2 should be 100")
+
+    character.level = 10
+    assert_equal(0, character.experience, "Experience should be set to 0 when character level is set")
+    assert_equal(3829, character.next_level, "Experience needed to reach level 11 should be 3829")
+
+    character.level = 6
+    assert_equal(0, character.experience, "Experience should be set to 0 when character level is set")
+    assert_equal(757, character.next_level, "Experience needed to reach level 7 should be 757")
+  end
+
+  # Tests various scenarios regarding experience loose and gain. Tests that the
+  # character's level and experience values are adjusted properly according to
+  # the experience change performed.
+  def test_set_experience
+    character = Character.new
+
+    assert_raises ArgumentError do
+      character.experience = "Hello"                # Not an integer
+    end
+
+    exp = Character::BASE_EXP / 10
+
+    # Set experience (less than next_level)
+    character.experience = exp
+    assert_equal(exp, character.experience, "Character's experience should be #{exp}")
+
+    # Increase experience (less than next_level)
+    character.experience += exp
+    assert_equal(exp*2, character.experience, "Character's experience should be #{exp*2}")
+
+    # Set experience (more than next level)
+    character.experience = Character::BASE_EXP + exp
+    assert_equal(2, character.level, "Character's level should be 2")
+    assert_equal(exp, character.experience, "Character's experience should be #{exp}")
+
+    # Decrease experience (enough to level down)
+    character.experience -= exp*2
+    assert_equal(1, character.level, "Character's level should be 1")
+    assert_equal(exp*9, character.experience, "Character's experience should be #{exp*9}")
+
+    # Increase experience (enough to level up)
+    character.experience += exp*2
+    assert_equal(2, character.level, "Character's level should be 2")
+    assert_equal(exp, character.experience, "Character's experience should be #{exp}")
+
+    # Gain a lot of experience
+    character.level = 1
+    character.experience += 5000
+    assert_equal(9, character.level, "Character's level should be 9")
+    assert_equal(89, character.experience, "Character's experience should be 89")
+
+    # Loose a lot of experience
+    character.level = 10
+    character.experience -= 5000
+    assert_equal(7, character.level, "Character's level should drop to 7")
+    assert_equal(390, character.experience, "Character's experience should be 390")
+
+    # Loose enough experience to go below level 1 (should stay at level 1)
+    character.level = 2
+    exp = character.next_level * 4
+    character.experience = -exp
+    assert_equal(1, character.level, "Character's level should be 1")
+    assert_equal(0, character.experience, "Character's experience should be 0")
+
+    # Trying to go over the level cap
+    character.level = Character::MAX_LEVEL
+    character.experience = character.next_level * 2
+    assert_equal(Character::MAX_LEVEL, character.level, "Character's level should be #{Character::MAX_LEVEL}")
+    assert_equal(character.next_level, character.experience, "Character's experience should be #{character.next_level}")
+
+    # Trying to go over the level cap
+    character.level = Character::MAX_LEVEL-1
+    character.experience = character.next_level * 4
+    assert_equal(Character::MAX_LEVEL, character.level, "Character's level should be #{Character::MAX_LEVEL}")
+    assert_equal(character.next_level, character.experience, "Character's experience should be #{character.next_level}")
+  end
 end
