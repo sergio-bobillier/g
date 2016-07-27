2154ee391d7ddc7d4c284ed8d88b8d47942a42d6
Sergio Bobillier C <sergio.bobillier@gmail.com>
2016-07-26 23:27:35 -0500

    Fixes minor typos in the Job class documentation.

--- job.rb
+++ job.rb
@@ -2,8 +2,8 @@ require_relative "character_modifier"
 
 # This class models a character Job. A Job, sometimes referred to as a Class
 # determines the main role the character plays in a party (as a nuker, a healer,
-# a defensive warrior, suport, etc.) This class contains a Stats object which
-# determines the bonuses the character gains by plaing a particular Job.
+# a defensive warrior, support, etc.) This class contains a Stats object which
+# determines the bonuses the character gains by playing a particular Job.
 #
 # NOTE: Originally this class was intended to be named "Class" but that would
 # monkey patch Ruby's Class class so let's avoid doing that for now ;)
