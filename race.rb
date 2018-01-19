d153c562eb2c8c965385e64bc7c2c5f4118186bb
Sergio Bobillier C <sergio.bobillier@gmail.com>
2018-01-19 09:18:06 -0500

    Lints the Race class.
    No logic changes were made but the code was moved around to satisfy
    Rubocop's suggestions.

--- race.rb
+++ race.rb
@@ -1,5 +1,5 @@
-require_relative "character_modifier"
-require_relative "elements"
+require_relative 'character_modifier'
+require_relative 'elements'
 
 # This class models a character race. A race contains a Stats object, this
 # object has the stats bonuses that should be given to a character of that
@@ -12,7 +12,7 @@ class Race < CharacterModifier
   #   of this race should receive bonuses on attack and AP gain.
   attr_reader :element
 
-  alias_method :elements, :element
+  alias elements element
 
   # Sets the race element (if given).
   def initialize(stats, element = nil)
@@ -26,27 +26,51 @@ class Race < CharacterModifier
   #
   # @param element [Symbol] The race's element.
   def element=(element)
-    unless element.nil?
-      unless element.is_a?(Symbol) || element.is_a?(Array)
-        raise ArgumentError.new("`element` should be a Symbol or an Array of Symbols")
-      end
+    unless element.is_a?(Symbol) || element.is_a?(Array) || element.nil?
+      raise ArgumentError, '`element` should be a Symbol or an Array of Symbols'
+    end
 
-      if element.is_a?(Array)
-        if element.length != element.uniq.length
-          raise ArgumentError.new("Elements in the array should be unique")
-        end
-
-        element.each do |elm|
-          raise ArgumentError.new("All elements of the array should be Symbols") unless elm.is_a?(Symbol)
-          raise ArgumentError.new("Unknown element '#{elm}'") unless ELEMENTS.include?(elm)
-        end
-      else
-        raise ArgumentError.new("Unknown element '#{element}'") unless ELEMENTS.include?(element)
-      end
+    if element.is_a?(Array)
+      validate_elements(element)
+    else
+      validate_element(element) unless element.nil?
     end
 
     @element = element
   end
 
-  alias_method :elements=, :element=
+  alias elements= element=
+
+  private
+
+  # Validates that no element is duplicated in the given array, that the array
+  # is compraised only of Symbols and that each of those Symbols is actually a
+  # valid element.
+  #
+  # @param elements [Array<Symbol>] The elements array.
+  # @raise [ArgumentError] If there is a duplicated element in the array, if
+  #   any of the elements in the array is not a Symbol or any of the elements is
+  #   unknown.
+  def validate_elements(elements)
+    if elements.length != elements.uniq.length
+      raise ArgumentError, 'Elements in the array should be unique'
+    end
+
+    elements.each do |element|
+      unless element.is_a?(Symbol)
+        raise ArgumentError, 'All elements of the array should be Symbols'
+      end
+
+      validate_element(element)
+    end
+  end
+
+  # Checks that the given element is valid.
+  #
+  # @param element [Symbol] The element.
+  # @raise [ArgumentError] If the element is unknown.
+  def validate_element(element)
+    return if ELEMENTS.include?(element)
+    raise ArgumentError, "Unknown element '#{element}'"
+  end
 end
