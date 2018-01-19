29326c77e8284454b9c10e72d57dd01ed7c445c1
Sergio Bobillier C <sergio.bobillier@gmail.com>
2018-01-19 14:50:34 -0500

    Lints the "Library".
    No logic changes were made but the code was moved around to satisfy
    Rubocop's suggestions.

--- library/jobs.rb
+++ library/jobs.rb
@@ -1,4 +1,4 @@
-require_relative "../job"
+require_relative '../job'
 
 # This module contains constants with all the available jobs. Usually these
 # would be loaded from a database but since this a conceptual game I want to
@@ -6,12 +6,10 @@ require_relative "../job"
 #
 # @author Sergio Bobillier C. <sergio.bobillier@gmail.com>
 module Jobs
-
-  WIZARD = Job.new(Stats.new({:con => 0, :str => 0, :dex => 0, :int => 5, :men => 1, :wit => 3}))
-  KNIGHT = Job.new(Stats.new({:con => 5, :str => 2, :dex => 0, :int => 0, :men => 2, :wit => 0}))
-  PRIEST = Job.new(Stats.new({:con => 0, :str => 0, :dex => 0, :int => 0, :men => 3, :wit => 3}))
-  ROGUE = Job.new(Stats.new({:con => 0, :str => 2, :dex => 5, :int => 0, :men => 0, :wit => 0}))
-  ARCHER = Job.new(Stats.new({:con => 0, :str => 2, :dex => 3, :int => 0, :men => 0, :wit => 0}))
-  MONK = Job.new(Stats.new({:con => 3, :str => 3, :dex => 1, :int => 0, :men => 1, :wit => 0}))
-
+  WIZARD = Job.new(Stats.new(con: 0, str: 0, dex: 0, int: 5, men: 1, wit: 3))
+  KNIGHT = Job.new(Stats.new(con: 5, str: 2, dex: 0, int: 0, men: 2, wit: 0))
+  PRIEST = Job.new(Stats.new(con: 0, str: 0, dex: 0, int: 0, men: 3, wit: 3))
+  ROGUE = Job.new(Stats.new(con: 0, str: 2, dex: 5, int: 0, men: 0, wit: 0))
+  ARCHER = Job.new(Stats.new(con: 0, str: 2, dex: 3, int: 0, men: 0, wit: 0))
+  MONK = Job.new(Stats.new(con: 3, str: 3, dex: 1, int: 0, men: 1, wit: 0))
 end
--- library/races.rb
+++ library/races.rb
@@ -1,4 +1,4 @@
-require_relative "../race"
+require_relative '../race'
 
 # This module contains a constant for each of the five races. Normally these
 # would be loaded from a database but since this is conceptual game I want to
@@ -6,18 +6,24 @@ require_relative "../race"
 #
 # @author Sergio Bobillier <sergio.bobillier@gmail.com>
 module Races
-  HUMAN = Race.new(Stats.new(
-    {:con => 3, :str => 3, :dex => 3, :int => 3, :men => 3, :wit => 3}), [:light, :dark])
+  HUMAN = Race.new(
+    Stats.new(con: 3, str: 3, dex: 3, int: 3, men: 3, wit: 3),
+    [:light, :dark]
+  )
 
-  ELF = Race.new(Stats.new(
-    {:con => 2, :str => 2, :dex => 4, :int => 4, :men => 4, :wit => 5}), :water)
+  ELF = Race.new(
+    Stats.new(con: 2, str: 2, dex: 4, int: 4, men: 4, wit: 5), :water
+  )
 
-  DARK_ELF = Race.new(Stats.new(
-    {:con => 1, :str => 2, :dex => 5, :int => 5, :men => 1, :wit => 4}), :wind)
+  DARK_ELF = Race.new(
+    Stats.new(con: 1, str: 2, dex: 5, int: 5, men: 1, wit: 4), :wind
+  )
 
-  DWARF = Race.new(Stats.new(
-    {:con => 5, :str => 4, :dex => 1, :int => 2, :men => 2, :wit => 2}), :earth)
+  DWARF = Race.new(
+    Stats.new(con: 5, str: 4, dex: 1, int: 2, men: 2, wit: 2), :earth
+  )
 
-  ORC = Race.new(Stats.new(
-    {:con => 4, :str => 5, :dex => 2, :int => 1, :men => 5, :wit => 1}), :fire)
+  ORC = Race.new(
+    Stats.new(con: 4, str: 5, dex: 2, int: 1, men: 5, wit: 1), :fire
+  )
 end
