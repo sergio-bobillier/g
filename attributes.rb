c600660451fc4e1af35011fe48d4251d2a2854b4
Sergio Bobillier C <sergio.bobillier@gmail.com>
2018-01-23 08:54:04 -0500

    Lints the Attributes class.
    No logic changes were made but the code was moved around to satisfy
    Rubocop's suggestions.

--- attributes.rb
+++ attributes.rb
@@ -1,70 +1,126 @@
-require_relative "stats"
+require_relative 'stats'
+
+# rubocop:disable Metrics/ClassLength
 
 # Models the attributes and stats of characters.
 #
 # @author Sergio Bobillier <sergio.bobillier@gmail.com>
 class Attributes
-
   # Character attributes with their types, bounds and other constraints. It Also
   # contains a lamda function to calculate the attribute value based on the
   # character stats and level.
   ATTRIBUTES = {
-    :defense => {:min => 0, :type => Integer,
-      :formula => lambda { |stats, level| (50*(stats[:con].to_f/10)*(1+(0.2*level))).floor }
-    },
-    :total_health => {:min => 0, :type => Integer,
-      :formula => lambda { |stats, level| ((stats[:con]-20)*100*(0.5*(level.to_f/4))+(stats[:con].to_f/8)*(300*(0.3*(level.to_f/2)))+(150-level)).floor }
-    },
-    :attack => {:min => 0, :type => Integer,
-      :formula => lambda { |stats, level| (40*(stats[:str].to_f/9)*(1+(0.3*level.to_f))).floor }
-    },
-    :critical_damage => {:min => 0, :type => Float,
-      :formula => lambda { |stats, level| (1+(level.to_f/50)+((stats[:str].to_f/500)*level.to_f*0.05)).round(2) }
-    },
-    :critical_rate => {:min => 0, :max => 1, :type => Float,
-      :formula => lambda { |stats, level| (((level.to_f/82*0.5)+stats[:dex].to_f/400)*0.7).round(2) }
-    },
-    :attack_speed => {:min => 0, :type => Integer,
-      :formula => lambda { |stats, level| ((stats[:dex]*5)+((stats[:dex]-15)*level.to_f*0.55)).floor }
-    },
-    :evasion => {:min => 0, :max => 1, :type => Float,
-      :formula => lambda { |stats, level| ((stats[:dex].to_f/200)+(level.to_f/500)).round(2) }
-    },
-    :accuracy => {:min => 0, :max => 1, :type => Float,
-      :formula => lambda { |stats, level| ((stats[:dex].to_f/37)+(level/1000)).round(2) }
-    },
-    :speed => {:min => 0, :max => 70, :type => Integer,
-      :formula => lambda { |stats, level| (20+((stats[:dex]-20)*2)+(level.to_f*0.1+stats[:dex].to_f*0.02)).to_i }
-    },
-    :magic_power => {:min => 0, :type => Integer,
-      :formula => lambda { |stats, level| (40*(stats[:int].to_f/6)*(1+(0.3*level.to_f))).floor }
-    },
-    :magic_critical_damage => {:min => 0, :type => Float,
-      :formula => lambda { |stats, level| (1+(level.to_f/55)+((stats[:int].to_f/490)*level.to_f*0.06)).round(2) }
-    },
-    :magic_defense => {:min => 0, :type => Integer,
-      :formula => lambda { |stats, level| (43*(stats[:men].to_f/7)*(1+(0.28*level.to_f))).floor }
-    },
-    :total_mana => {:min => 0, :type => Integer,
-      :formula => lambda { |stats, level| ((stats[:men]-20)*100*(0.5*(level.to_f/4))+(stats[:men].to_f/8)*(300*(0.2*(level.to_f/5)))+(150-level)).floor }
-    },
-    :magic_critical_rate => {:min => 0, :max => 1, :type => Float,
-      :formula => lambda { |stats, level| (((level.to_f/82*0.5)+stats[:wit].to_f/400)*0.4).round(2) }
-    },
-    :magic_accuracy => {:min => 0, :max => 1, :type => Float,
-      :formula => lambda { |stats, level| (((20+(stats[:wit].to_f/3))/38)+(level.to_f/400)).round(2) }
-    },
-    :magic_evasion => {:min => 0, :max => 1, :type => Float,
-      :formula => lambda { |stats, level| ((stats[:wit].to_f/175)+(level.to_f/400)).round(2) }
-    },
-    :casting_speed => {:min => 0, :type => Integer,
-      :formula => lambda { |stats, level| ((stats[:wit].to_f*12.54)+((stats[:wit].to_f-20)*0.65*level.to_f)+(3*level.to_f)).floor }
-    },
+    defense: { min: 0, type: Integer,
+               formula: lambda { |stats, level|
+                 (50 * (stats[:con].to_f / 10) *
+                 (1 + (0.2 * level))).floor
+               } },
+
+    total_health: { min: 0, type: Integer,
+                    formula: lambda { |stats, level|
+                      ((stats[:con] - 20) * 100 *
+                      (0.5 * (level.to_f / 4)) +
+                      (stats[:con].to_f / 8) *
+                      (300 * (0.3 * (level.to_f / 2))) +
+                      (150 - level)).floor
+                    } },
+
+    attack: { min: 0, type: Integer,
+              formula: lambda { |stats, level|
+                (40 * (stats[:str].to_f / 9) *
+                (1 + (0.3 * level.to_f))).floor
+              } },
+
+    critical_damage: { min: 0, type: Float,
+                       formula: lambda { |stats, level|
+                         (1 + (level.to_f / 50) +
+                         ((stats[:str].to_f / 500) *
+                         level.to_f * 0.05)).round(2)
+                       } },
+
+    critical_rate: { min: 0, max: 1, type: Float,
+                     formula: lambda { |stats, level|
+                       (((level.to_f / 82 * 0.5) +
+                       stats[:dex].to_f / 400) * 0.7).round(2)
+                     } },
+
+    attack_speed: { min: 0, type: Integer,
+                    formula: lambda { |stats, level|
+                      ((stats[:dex] * 5) +
+                      ((stats[:dex] - 15) * level.to_f * 0.55)).floor
+                    } },
+
+    evasion: { min: 0, max: 1, type: Float,
+               formula: lambda { |stats, level|
+                 ((stats[:dex].to_f / 200) + (level.to_f / 500)).round(2)
+               } },
+
+    accuracy: { min: 0, max: 1, type: Float,
+                formula: lambda { |stats, level|
+                  ((stats[:dex].to_f / 37) + (level / 1000)).round(2)
+                } },
+
+    speed: { min: 0, max: 70, type: Integer,
+             formula: lambda { |stats, level|
+               (20 + ((stats[:dex] - 20) * 2) +
+               (level.to_f * 0.1 + stats[:dex].to_f * 0.02)).to_i
+             } },
+
+    magic_power: { min: 0, type: Integer,
+                   formula: lambda { |stats, level|
+                     (40 * (stats[:int].to_f / 6) *
+                     (1 + (0.3 * level.to_f))).floor
+                   } },
+
+    magic_critical_damage: { min: 0, type: Float,
+                             formula: lambda { |stats, level|
+                               (1 + (level.to_f / 55) +
+                               ((stats[:int].to_f / 490) *
+                               level.to_f * 0.06)).round(2)
+                             } },
+
+    magic_defense: { min: 0, type: Integer,
+                     formula: lambda { |stats, level|
+                       (43 * (stats[:men].to_f / 7) *
+                       (1 + (0.28 * level.to_f))).floor
+                     } },
+
+    total_mana: { min: 0, type: Integer,
+                  formula: lambda { |stats, level|
+                    ((stats[:men] - 20) * 100 * (0.5 * (level.to_f / 4)) +
+                    (stats[:men].to_f / 8) * (300 * (0.2 * (level.to_f / 5))) +
+                    (150 - level)).floor
+                  } },
+
+    magic_critical_rate: { min: 0, max: 1, type: Float,
+                           formula: lambda { |stats, level|
+                             (((level.to_f / 82 * 0.5) +
+                             stats[:wit].to_f / 400) * 0.4).round(2)
+                           } },
+
+    magic_accuracy: { min: 0, max: 1, type: Float,
+                      formula: lambda { |stats, level|
+                        (((20 + (stats[:wit].to_f / 3)) / 38) +
+                        (level.to_f / 400)).round(2)
+                      } },
+
+    magic_evasion: { min: 0, max: 1, type: Float,
+                     formula: lambda { |stats, level|
+                       ((stats[:wit].to_f / 175) +
+                       (level.to_f / 400)).round(2)
+                     } },
+
+    casting_speed: { min: 0, type: Integer,
+                     formula: lambda { |stats, level|
+                       ((stats[:wit].to_f * 12.54) +
+                       ((stats[:wit].to_f - 20) * 0.65 * level.to_f) +
+                       (3 * level.to_f)).floor
+                     } },
 
     # Transient attributes
-    :health => {:min => 0, :max => :total_health, :type => Integer},
-    :mana => {:min => 0, :max => :total_mana, :type => Integer}
-  }
+    health: { min: 0, max: :total_health, type: Integer },
+    mana: { min: 0, max: :total_mana, type: Integer }
+  }.freeze
 
   # Initializes the @attributes instance variable with the minimum value for
   # each of the character's attributes. Then, if a stats object is given
@@ -73,10 +129,17 @@ class Attributes
   # @param stats [Stats] A stats object used to calculate attribute values.
   # @param level [Integer] The character's level
   def initialize(stats = nil, level = 1)
-    raise ArgumentError.new("Expecting Stats but got #{stats.class}") unless stats.is_a?(Stats) || stats.nil?
-    raise ArgumentError.new("Expecting an Integer but got #{level.class}") unless level.is_a?(Integer)
+    unless stats.is_a?(Stats) || stats.nil?
+      raise ArgumentError, 'Expecting `stats` to be an instance of `Stats` '\
+                           "but got #{stats.class}"
+    end
 
-    @attributes = {};
+    unless level.is_a?(Integer)
+      raise ArgumentError, 'Expecting `level` to be an Integer but got'\
+                           "#{level.class}"
+    end
+
+    @attributes = {}
     ATTRIBUTES.each do |attribute, properties|
       @attributes[attribute] = properties[:min]
     end
@@ -114,25 +177,20 @@ class Attributes
   # @param reset_transient_attributes [Boolean] If true transient attributes
   #   like health and mana will be reset to their maximum.
   def calculate_attributes(stats, level, reset_transient_attributes = false)
-    @attributes.each do |attribute, value|
+    @attributes.keys.each do |attribute|
       next unless ATTRIBUTES[attribute][:formula]
-      set_attribute(attribute, ATTRIBUTES[attribute][:formula].call(stats, level))
+      set_attribute(attribute,
+                    ATTRIBUTES[attribute][:formula].call(stats, level))
     end
 
-    if reset_transient_attributes
-      set_attribute(:health, @attributes[:total_health])
-      set_attribute(:mana, @attributes[:total_mana])
-    else
-      if @attributes[:health] > @attributes[:total_health]
-        @attributes[:health] = @attributes[:total_health]
-      end
-      if @attributes[:mana] > @attributes[:total_mana]
-        @attributes[:mana] = @attributes[:total_mana]
-      end
-    end
+    return unless reset_transient_attributes
+    set_attribute(:health, @attributes[:total_health])
+    set_attribute(:mana, @attributes[:total_mana])
   end
 
   private
+
+  # rubocop:disable Style/GuardClause
 
   # Validates that the given attribute name is a Symbol and the name of a known
   # attribute.
@@ -141,9 +199,18 @@ class Attributes
   # @raise [ArgumentError] If the given attribute name is not a symbol or is not
   #   the name of a known attribute.
   def validate_attribute(attribute)
-    raise ArgumentError.new("Attribute name expected to be a Symbol, #{attribute.class} given instead") unless attribute.is_a?(Symbol)
-    raise ArgumentError.new("Unrecognized attribute #{attribute}") unless ATTRIBUTES.has_key?(attribute)
+    unless attribute.is_a?(Symbol)
+      raise ArgumentError,
+            "Attribute name expected to be a Symbol, #{attribute.class} given "\
+            'instead'
+    end
+
+    unless ATTRIBUTES.key?(attribute)
+      raise ArgumentError, "Unrecognized attribute #{attribute}"
+    end
   end
+
+  # rubocop:enable Style/GuardClause
 
   # Sets the specified attribute to the given value.
   #
@@ -157,29 +224,56 @@ class Attributes
 
     # Type validation
     if properties[:type] && !value.is_a?(properties[:type])
-      raise ArgumentError.new("Expected #{properties[:type]} but got #{value.class} instead")
+      raise ArgumentError,
+            "Expected #{properties[:type]} but got #{value.class} instead"
     end
 
+    value = validate_limits(properties, value)
+    @attributes[attribute] = value
+
+    # Adjust transient attributes if their totals decrease.
+    return unless [:total_health, :total_mana].include? attribute
+    update_transient_attribute(attribute)
+  end
+
+  def update_transient_attribute(attribute)
+    transient = attribute.to_s[6..-1].to_sym # Remove "total_"
+    return if @attributes[transient] < @attributes[attribute]
+    @attributes[transient] = @attributes[attribute]
+  end
+
+  # rubocop:disable Style/RedundantReturn
+
+  # Validates that the given value is within the limits for the attribute.
+  #
+  # @param properties [Hash] The attribute's properties.
+  # @param value The value for the attribute.
+  # @return The same value if it was within the limits or the adjusted value if
+  #   was not.
+  def validate_limits(properties, value)
     # Minimum
     if properties[:min]
-      min = (properties[:min].is_a?(Symbol) ? @attributes[properties[:min]] : properties[:min])
+      min = limit_value properties, :min
       value = min if value < min
     end
 
     # Maximum
     if properties[:max]
-      max = (properties[:max].is_a?(Symbol) ? @attributes[properties[:max]] : properties[:max])
+      max = limit_value properties, :max
       value = max if value > max
     end
 
-    @attributes[attribute] = value
+    return value
+  end
 
-    # Adjust transient attributes if their totals decrease.
-    if attribute == :total_health || attribute == :total_mana
-      transient = attribute.to_s[6..-1].to_sym                 # Remove "total_"
-      if @attributes[transient] > @attributes[attribute]
-        @attributes[transient] = @attributes[attribute]
-      end
-    end
+  # rubocop:disable Metrics/LineLength
+
+  # Returns the specified limit value for the attribute.
+  #
+  # @param properties [Hash] The attribute's properties.
+  # @param limit [Symbol] The limit value to get, either `:min` or `:max`
+  # @return The limit value.
+  def limit_value(properties, limit)
+    properties[limit].is_a?(Symbol) ? @attributes[properties[limit]] : properties[limit]
   end
 end
