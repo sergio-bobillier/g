143cbe924255beb26011c3dc614a3253f1a0d05a
Sergio Bobillier C <sergio.bobillier@gmail.com>
2018-11-16 15:36:50 -0500

    Moves the CharacterModifier tests to a shared examples group.
    - These tests will later be used in other classes that inherit
      from CharacterModifier so having them in a shared examples
      group is probably a good idea.

--- spec/character_modifier_spec.rb
+++ spec/character_modifier_spec.rb
@@ -1,36 +1,8 @@
 # frozen_string_literal: true
 
 require_relative '../character_modifier'
+require_relative 'support/character_modifier_behavior.rb'
 
 RSpec.describe CharacterModifier do
-  describe '#initialize' do
-    context 'when no `stats` object is given' do
-      it 'raises an error' do
-        message = 'wrong number of arguments (given 0, expected 1)'
-        expect { described_class.new }.to raise_error ArgumentError, message
-      end
-    end
-
-    context 'when something besides an `Stats` object is given' do
-      it 'raises an error' do
-        message = '`stats` should be an instance of Stats'
-        expect { described_class.new('Hello') }.to raise_error ArgumentError,
-                                                               message
-      end
-    end
-
-    context 'when a valid Stats object is given' do
-      let(:stats) { Stats.new(con: 10, str: 8, dex: 6, int: 4, men: 2, wit: 0) }
-      subject { described_class.new(stats) }
-
-      it 'has the correct stats' do
-        expect(subject.stats[:con]).to eq 10
-        expect(subject.stats[:str]).to eq 8
-        expect(subject.stats[:dex]).to eq 6
-        expect(subject.stats[:int]).to eq 4
-        expect(subject.stats[:men]).to eq 2
-        expect(subject.stats[:wit]).to eq 0
-      end
-    end
-  end
+  it_behaves_like 'a CharacterModifier'
 end
---
+++ spec/support/character_modifier_behavior.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+RSpec.shared_examples_for 'a CharacterModifier' do
+  describe '#initialize' do
+    context 'when no `stats` object is given' do
+      it 'raises an error' do
+        message = 'wrong number of arguments (given 0, expected 1)'
+        expect { described_class.new }.to raise_error ArgumentError, message
+      end
+    end
+
+    context 'when something besides an `Stats` object is given' do
+      it 'raises an error' do
+        message = '`stats` should be an instance of Stats'
+        expect { described_class.new('Hello') }.to raise_error ArgumentError,
+                                                               message
+      end
+    end
+
+    context 'when a valid Stats object is given' do
+      let(:stats) { Stats.new(con: 10, str: 8, dex: 6, int: 4, men: 2, wit: 0) }
+      subject { described_class.new(stats) }
+
+      it 'has the correct stats' do
+        expect(subject.stats[:con]).to eq 10
+        expect(subject.stats[:str]).to eq 8
+        expect(subject.stats[:dex]).to eq 6
+        expect(subject.stats[:int]).to eq 4
+        expect(subject.stats[:men]).to eq 2
+        expect(subject.stats[:wit]).to eq 0
+      end
+    end
+  end
+end
